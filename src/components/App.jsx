baf48b05565b2d839fce83b20c0ed9b98b32ed8c
TatianaPrkhmnk <tatiashka11@gmail.com>
2023-09-19 14:19:20 +0300

    react-bootstrap, nav-bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+
+          <Form className="d-flex">
+            <FormControl type="search" placeholder="Search" aria-label="Search" />
+            <Button variant="outline-success" type="submit">Search</Button>
+          </Form>
+    
+      </Container>
+    </Navbar>
   );
 };
